cf529941c72ba4fab1f650ca65c7d0d62e70e2df
Shazwi Suwandi <supershazwi@gmail.com>
2015-08-29 18:55:47 +0800

    trips

--- EazieBizie/www/js/app.js
+++ EazieBizie/www/js/app.js
@@ -111,7 +111,7 @@ var starter = angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/login');
+  $urlRouterProvider.otherwise('/app/trips');
   
   $httpProvider.defaults.useXDomain = true;
   $httpProvider.defaults.withCredentials = true;
