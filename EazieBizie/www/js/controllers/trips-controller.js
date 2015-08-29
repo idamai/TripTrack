681bc89468fe870395c2c593bf77d5b847953fa1
Ignatius Damai <ignatiusdamai@gmail.com>
2015-08-29 13:49:45 +0800

    Cleaned up test codes

--- EazieBizie/www/js/controllers/trips-controller.js
+++ EazieBizie/www/js/controllers/trips-controller.js
@@ -1,7 +1,3 @@
-var scope;
 starter.controller('TripsCtrl', function($scope, TripsService) {
   $scope.trips = TripsService.getTrips(0);
-  console.log('This is loaded');
-  $scope.test = 'This is loaded';
-  scope = $scope;
 });
